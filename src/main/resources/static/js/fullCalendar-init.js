a08fbe93bbdbbf31ba9afd6d7641c365d792f800
nugos <ldv@ldv.com.ua>
2022-11-06 15:16:48 +0200

    refactor style.css add timeGridSevenDay, navLinkDayClick, navLinkWeekClick in fullCalendar-init.js

--- src/main/resources/static/js/fullCalendar-init.js
+++ src/main/resources/static/js/fullCalendar-init.js
@@ -1,11 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
-        header: {
-            left: 'prev,next today',
+
+        headerToolbar: {
+            left: 'prev,today,next',
             center: 'title',
-            right: 'month,agendaWeek,agendaDay'
+            right: 'dayGridMonth,timeGridSevenDay,timeGridDay'
         },
+        views: {
+            timeGridSevenDay: {
+                type: 'timeGrid',
+                duration: { days: 7 },
+                buttonText: 'Тиждень'
+            }
+        },
+        navLinks: true,
+        navLinkDayClick: function(date, jsEvent) {
+            console.log('day', date.toISOString());
+            console.log('coords', jsEvent.pageX, jsEvent.pageY);
+        },
+        navLinkWeekClick: function(weekStart, jsEvent) {
+            console.log('week start', weekStart.toISOString());
+            console.log('coords', jsEvent.pageX, jsEvent.pageY);
+        },
+        themeSystem: 'bootstrap5',
         selectable: true,
         selectHelper: true,
         initialView: 'dayGridMonth',
